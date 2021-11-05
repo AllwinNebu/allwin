10749a2f4ba68e4f48ca6497a655626063b1f9a8
AllwinNebu <allwinnebu@gmail.com>
2021-11-05 13:05:01 +0530

    new

--- c++/struct.cpp
+++ c++/struct.cpp
@@ -9,7 +9,7 @@ struct sales_data
     char name[20];
     float amt[12];
     float avg;
-}
+};
 int main()
 {
     sales_data s[20];
@@ -22,6 +22,27 @@ int main()
       cout <<"Enter the detail of the salesman  " <<i+1;
       cout<<"\nSalesman code: ";
       cin>>s[i].code;
-      f   
+      fflush(stdin);
+      cout<<"Name: ";
+      gets(s[i].name);
+      cout<<"Amout of sales in 12 months: ";
+      for(sum=0,j=0; j<12; j++)
+      {
+        cin>>s[i].amt[j];
+        sum=sum+s[i].amt[j];
+      }
+      s[i].avg=sum/12;
     }
+    cout<<"\t\tDetail of Sales\n";
+    cout<<"Code \t\tName \t\tAverage Sales\n";
+    for(i=0;i<n;i++)
+    {
+      cout<<setw(4)<<s[i].code<<setw(15)<<s[i].name;
+      for (j=0;j<12;j++)
+      {
+        cout<<setw(4)<<s[i].amt[j];
+      }
+      cout<<s[i].avg<<'\n';
+    }
+    return 0;
 }
