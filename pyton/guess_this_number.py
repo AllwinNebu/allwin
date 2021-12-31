d884229736f380c0c3b2edf7e69b018ca147b8b4
AllwinNebu <allwinnebu@gmail.com>
2022-01-01 00:22:54 +0530

    idk

---
+++ pyton/guess_this_number.py
@@ -0,0 +1,18 @@
+import random
+while 1>0:
+    cho=input("type start to continue or anything else to exit : ")
+    if cho =="start":
+        top_range = input("Type your guess : ")
+
+        if top_range.isdigit():
+            top_range = int(top_range)
+            if top_range <=0:
+             print("type a number greather than 0 asshole")
+             quit()
+        else :
+         print("you fucking donkey this is a numer guess type a number idiot")
+         quit()  
+         random_number=random.randint( 0,top_range)
+         print(random_number)   
+    else:
+        quit()    
