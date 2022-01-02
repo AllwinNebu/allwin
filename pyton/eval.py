51122ce83a323a84c377dc78499ed38f69ea706f
AllwinNebu <allwinnebu@gmail.com>
2022-01-02 22:41:37 +0530

    eval

---
+++ pyton/eval.py
@@ -0,0 +1,9 @@
+while 1>0:
+    cho=input("Do you want to contine : ")
+    if cho =="yes":
+        x = input("write a maths problem : ") 
+        print("your answer is : ", eval(x))
+    else:
+        quit()    
+
+
