64b533f0a513ff7c7d2b51176fbde1cb9ccef23a
AllwinNebu <86886342+AllwinNebu@users.noreply.github.com>
2024-01-30 13:08:29 +0530

    python

---
+++ pyton/lab/palindrome.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the number : "))
+temp=n
+rev = 0
+while temp>0:
+    rem=temp%10
+    rev=(rev*10)+rem
+    temp=temp//10
+
+if n==rev:
+    print("palindrome")
+else:
+    print("Not palindrome")
